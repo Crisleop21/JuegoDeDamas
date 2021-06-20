6d9e39767ca904757c8b29d4aa976fce730b5ff3
Crisleop21 <85314436+Crisleop21@users.noreply.github.com>
2021-06-20 17:40:50 -0600

    Nuevas clases celda u fichas

--- src/principal/Main.java
+++ src/principal/Main.java
@@ -3,11 +3,11 @@ package src.principal;
 import src.tablero.Tablero;
 
 public class MostrarTablero {
+   
     public static void main(String[] args) {
-        String ANSI_BLANCO = "176 ";
-        String ANSI_NEGRO = "178";
         
-        Tablero tablero = new Tablero(ANSI_BLANCO,ANSI_NEGRO);
+       
+        Tablero tablero = new Tablero();
         
         tablero.imprimeTablero();
     }
--- src/tablero/Casilla.java
+++
@@ -1,7 +0,0 @@
-package src.tablero;
-
-public class Casilla {
-    
-
-    
-}
