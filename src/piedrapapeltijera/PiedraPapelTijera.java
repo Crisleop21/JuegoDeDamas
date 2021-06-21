63f3fa620d1e844bdcc244ed96240fe84b7159a9
Crisleop21 <85314436+Crisleop21@users.noreply.github.com>
2021-06-21 16:08:42 -0600

    Se agrego Piedra, Papel y tijera

--- src/piedrapapeltijera/PiedraPapelTijera.java
+++ src/piedrapapeltijera/PiedraPapelTijera.java
@@ -2,13 +2,62 @@ package src.piedrapapeltijera;
 
 import java.util.Scanner;
 
+/*import src.jugadores.Jugador;
 import src.jugadores.TablaJugadores;
+import src.tablero.Tablero;
+import java.util.*;*/
+ 
+public class PiedraPapelTijera{   
 
-public class PiedraPapelTijera {
-    Scanner scanner = new Scanner(System.in);
-   
-   
-    public PiedraPapelTijera(){
+    public void PiedraPapelTijera(){
         
+        System.out.println("Juego piedra-papel-tijera");
+        Scanner scanner = new Scanner(System.in);
+
+        int seleccionCompu = (int)(Math.random() * 3) + 1;
+        System.out.println("La computadora ya escogio...");
+
+        System.out.print("Indique su seleccion [1=Piedra, 2=Papel, 3=Tijera]: ");
+        int seleccionUsuario = scanner.nextInt();
+
+    System.out.print("La computadora habia escogido: ");
+    switch ( seleccionCompu )
+    {
+       case 1:
+          System.out.println("Piedra");
+          switch ( seleccionUsuario )
+          {
+             case 1: System.out.println("Empate!"); break;
+             case 2: System.out.println("Usted gana!"); break;
+             case 3: System.out.println("La computadora gana!"); break;
+          }
+          break;
+
+       case 2:
+          System.out.println("Papel");
+          switch ( seleccionUsuario )
+          {
+             case 1: System.out.println("La computadora gana!"); break;
+             case 2: System.out.println("Empate!"); break;
+             case 3: System.out.println("Usted gana!"); break;
+          }
+          break;
+
+       case 3:
+          System.out.println("Tijera");
+          switch ( seleccionUsuario )
+          {
+             case 1: System.out.println("Usted gana!"); break;
+             case 2: System.out.println("La computadora gana!"); break;
+             case 3: System.out.println("Empate!"); break;
+          }
+          break;
     }
+
+    }
+              
+    
+
 }
+
+
